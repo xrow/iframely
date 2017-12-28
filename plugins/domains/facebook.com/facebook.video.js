a7581980ee43bfac7be43ca5ec7b8997568b8ac9
Ivan Paramonau <i.paramonau@gmail.com>
2017-12-28 12:55:51 -0500

    domains: add aspect for fb videos

--- plugins/domains/facebook.com/facebook.video.js
+++ plugins/domains/facebook.com/facebook.video.js
@@ -17,40 +17,55 @@ module.exports = {
         var link = {
             type: CONFIG.T.text_html,
             rel: [CONFIG.R.ssl, CONFIG.R.html5],
-        }; 
+        };
+
+        var autoplay = null;
 
         if (/comment_id=\d+/i.test(url) && !/class=\"fb\-comment\-embed\"/.test(html)) {
             // thank you FB for not working with comments
             // https://developers.facebook.com/docs/plugins/embedded-comments
 
-            var width = options.maxWidth || options.getProviderOptions('facebook.width', 640);            
+            var width = options.maxWidth || options.getProviderOptions('facebook.width', 640);
             html = html.replace(/class=\"fb\-video\"/, 
                 'class="fb-comment-embed" data-include-parent="'
                 + (options.getProviderOptions('facebook.include_comment_parent') || options.getProviderOptions(CONFIG.O.full) ? 'true' : 'false') + '"'
                 + (/data\-width=/i.test(html) ? '' : ' data-width="' + width + '"')
                 ); 
             link.rel.push (CONFIG.R.app);
+            
+        } else if (/class=\"fb\-post\"/i.test(html)) {
+            link.rel.push (CONFIG.R.app);
+
         } else {
             link.rel.push (CONFIG.R.player);
 
             if (options.getProviderOptions(CONFIG.O.full) && !/data\-show\-text/.test(html)) {
                 html = html.replace(/class=\"fb\-video\"/, 'class="fb-video" data-show-text="true"');
+            } else {
+
+                var aspect = oembed.height && oembed.width ? oembed.width / oembed.height : null;
+                link['aspect-ratio'] = aspect;
+
+                if (oembed.url) {
+                    autoplay = {
+                        href: 'https://www.facebook.com/plugins/video.php?href=' + encodeURIComponent(oembed.url) + '&autoplay=1',
+                        type: CONFIG.T.text_html,
+                        rel: [CONFIG.R.player, CONFIG.R.html5, CONFIG.R.autoplay],
+                        "aspect-ratio": aspect
+                    };
+                }
             }
-            // aspect-ratio is incorrect in oEmbed responses.
-            // Let's skip it and check og image instead, if any
-            // link['aspect-ratio'] = oembed.thumbnail_height && oembed.thumbnail_width ? oembed.thumbnail_width / oembed.thumbnail_height : 16/9
 
         }
 
         link.html = html;
 
-        return link;
+        return autoplay ? [link, autoplay] : link;
     },
 
     tests: [
         "http://www.facebook.com/video/video.php?v=4253262701205&set=vb.1574932468&type=2",
         "http://www.facebook.com/photo.php?v=4253262701205&set=vb.1574932468&type=2&theater",
-        "https://www.facebook.com/video.php?v=10152309398358392&fref=nf",
         "https://www.facebook.com/video.php?v=4392385966850",
         "https://www.facebook.com/joe.yu.94/videos/10206321173378788/",
         "https://business.facebook.com/KMPHFOX26/videos/10154356403004012/",
